13cc93731681b8042502c6d83ac26032ce7031a7
bsgabriel <gabrielbs22@icloud.com>
2025-06-18 00:46:42 -0300

    feature: implementação spring security

---
+++ src/main/java/com/bsg/trustedone/service/CustomUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.bsg.trustedone.service;
+
+import com.bsg.trustedone.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = this.userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("aaaaa"));
+
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return List.of();
+            }
+
+            @Override
+            public String getPassword() {
+                return user.getPassword();
+            }
+
+            @Override
+            public String getUsername() {
+                return user.getEmail();
+            }
+        };
+    }
+}
