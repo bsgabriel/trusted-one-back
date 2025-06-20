9b33bebf766e198c59fa972a371e4100de1e187e
bsgabriel <gabrielbs22@icloud.com>
2025-06-20 13:01:46 -0300

    feature: Criado dto para representar UserDetails

--- src/main/java/com/bsg/trustedone/service/CustomUserDetailsService.java
+++ src/main/java/com/bsg/trustedone/service/CustomUserDetailsService.java
@@ -1,41 +1,23 @@
 package com.bsg.trustedone.service;
 
+import com.bsg.trustedone.mapper.UserMapper;
 import com.bsg.trustedone.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private final UserMapper userMapper;
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Invalid email or password"));
-
-        return new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return List.of();
-            }
-
-            @Override
-            public String getPassword() {
-                return user.getPassword();
-            }
-
-            @Override
-            public String getUsername() {
-                return user.getEmail();
-            }
-        };
+        return userMapper.toUserDetailDto(user);
     }
 }
