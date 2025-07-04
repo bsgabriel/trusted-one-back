8e10c66dfe2e984f2198bddfc507c0ec53a9ff99
bsgabriel <gabrielbs22@icloud.com>
2025-07-04 12:54:47 -0300

    refactor: Adicionado anotação de construtores para instanciação de classe

--- src/main/java/com/bsg/trustedone/dto/AccountCreationDto.java
+++ src/main/java/com/bsg/trustedone/dto/AccountCreationDto.java
@@ -3,11 +3,15 @@ package com.bsg.trustedone.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountCreationDto {
 
     @NotBlank(message = "Name is required")
