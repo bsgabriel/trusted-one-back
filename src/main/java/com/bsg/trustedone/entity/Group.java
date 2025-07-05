9e69a837254bfaf6805880ec947a3530075577cb
bsgabriel <gabrielbs22@icloud.com>
2025-07-05 01:45:04 -0300

    fix: Ajustado construtores de Group

--- src/main/java/com/bsg/trustedone/entity/Group.java
+++ src/main/java/com/bsg/trustedone/entity/Group.java
@@ -1,12 +1,16 @@
 package com.bsg.trustedone.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "groups")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Group {
 
