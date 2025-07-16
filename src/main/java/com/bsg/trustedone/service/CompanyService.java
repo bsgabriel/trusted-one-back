2855dd64453a6d5f758683654fcbe299c949a616
bsgabriel <gabrielbs22@icloud.com>
2025-07-16 01:42:14 -0300

    refactor: Melhorado método de atualização de empresa

--- src/main/java/com/bsg/trustedone/service/CompanyService.java
+++ src/main/java/com/bsg/trustedone/service/CompanyService.java
@@ -3,6 +3,7 @@ package com.bsg.trustedone.service;
 import com.bsg.trustedone.dto.CompanyCreationDto;
 import com.bsg.trustedone.dto.CompanyDto;
 import com.bsg.trustedone.exception.ResourceAlreadyExistsException;
+import com.bsg.trustedone.exception.ResourceNotFoundException;
 import com.bsg.trustedone.exception.UnauthorizedAccessException;
 import com.bsg.trustedone.factory.CompanyFactory;
 import com.bsg.trustedone.mapper.CompanyMapper;
@@ -12,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -63,22 +63,18 @@ public class CompanyService {
         companyRepository.delete(company);
     }
 
-    public Optional<CompanyDto> updateCopmany(CompanyCreationDto request, Long companyId) {
+    public CompanyDto updateCopmany(CompanyCreationDto request, Long companyId) {
         companyValidator.validateCompanyUpdate(request);
 
-        return companyRepository.findById(companyId)
-                .map(company -> {
-                    var loggedUserId = userService.getLoggedUser().getUserId();
+        var company = companyRepository.findById(companyId).orElseThrow(() -> new ResourceNotFoundException("Company not found"));
 
-                    if (!company.getUserId().equals(loggedUserId)) {
-                        throw new UnauthorizedAccessException("An error ocurred while updating company");
-                    }
+        if (!company.getUserId().equals(userService.getLoggedUser().getUserId())) {
+            throw new UnauthorizedAccessException("An error ocurred while updating company");
+        }
 
-                    company.setName(request.getName());
-                    company.setImage(request.getImage());
-                    return companyMapper.toDto(companyRepository.save(company));
-                });
+        company.setName(request.getName());
+        company.setImage(request.getImage());
+        return companyMapper.toDto(companyRepository.save(company));
     }
-
 
 }
