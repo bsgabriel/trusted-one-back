a1c2e47c013c6e1738615c2190843a625e6dabf0
bsgabriel <gabrielbs22@icloud.com>
2025-08-08 02:03:04 -0300

    feature: Criado novos métodos para criação de entidade Company e Group

--- src/main/java/com/bsg/trustedone/factory/CompanyFactory.java
+++ src/main/java/com/bsg/trustedone/factory/CompanyFactory.java
@@ -1,6 +1,7 @@
 package com.bsg.trustedone.factory;
 
 import com.bsg.trustedone.dto.CompanyCreationDto;
+import com.bsg.trustedone.dto.CompanyDto;
 import com.bsg.trustedone.entity.Company;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,14 @@ import org.springframework.stereotype.Component;
 public class CompanyFactory {
 
     public Company createEntity(CompanyCreationDto company, Long userId) {
+        return Company.builder()
+                .userId(userId)
+                .name(company.getName())
+                .image(company.getImage())
+                .build();
+    }
+
+    public Company createEntity(CompanyDto company, Long userId) {
         return Company.builder()
                 .userId(userId)
                 .name(company.getName())
--- src/main/java/com/bsg/trustedone/factory/GroupFactory.java
+++ src/main/java/com/bsg/trustedone/factory/GroupFactory.java
@@ -1,6 +1,7 @@
 package com.bsg.trustedone.factory;
 
 import com.bsg.trustedone.dto.GroupCreationDto;
+import com.bsg.trustedone.dto.GroupDto;
 import com.bsg.trustedone.dto.UserDto;
 import com.bsg.trustedone.entity.Group;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,14 @@ import org.springframework.stereotype.Component;
 public class GroupFactory {
 
     public Group createEntity(GroupCreationDto groupCreationDto, UserDto userDto) {
+        return Group.builder()
+                .userId(userDto.getUserId())
+                .name(groupCreationDto.getName())
+                .description(groupCreationDto.getDescription())
+                .build();
+    }
+
+    public Group createEntity(GroupDto groupCreationDto, UserDto userDto) {
         return Group.builder()
                 .userId(userDto.getUserId())
                 .name(groupCreationDto.getName())
