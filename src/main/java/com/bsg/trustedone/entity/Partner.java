6f12643bd8845bb73c86bf7d964c9db5f62aed03
bsgabriel <bsgabriel.dev@gmail.com>
2026-01-15 20:24:15 -0300

    refactor: Alterado anotações para entidade Partner

--- src/main/java/com/bsg/trustedone/entity/Partner.java
+++ src/main/java/com/bsg/trustedone/entity/Partner.java
@@ -1,16 +1,14 @@
 package com.bsg.trustedone.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
